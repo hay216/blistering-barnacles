7df15fe40bf7f38613b6f434abd28ffec845d5e3
Steve Lane <lane.s@unimelb.edu.au>
2017-05-05 15:22:53 +1000

    Updating stuff

--- scripts/post-process.R
+++ scripts/post-process.R
@@ -4,13 +4,14 @@
 ## Author: Steve Lane
 ## Date: Thursday, 04 May 2017
 ## Synopsis: Post process the output from the regression models
-## Time-stamp: <2017-05-04 20:49:04 (slane)>
+## Time-stamp: <2017-05-05 14:23:13 (slane)>
 ################################################################################
 ################################################################################
 ## Add github packages using gitname/reponame format
 source("../scripts/imputation-functions.R")
-packages <- c("tidyr", "dplyr", "rstan", "loo", "ggplot2")
+packages <- c("tidyr", "dplyr", "rstan", "loo", "ggplot2", "RColorBrewer")
 ipak(packages)
+## This is set for readable text when included at half page width.
 theme_set(theme_bw())
 m0 <- readRDS("../data/censored-mle-m0.rds")
 m1 <- readRDS("../data/censored-mle-m1.rds")
@@ -18,6 +19,79 @@ m2 <- readRDS("../data/censored-mle-m2.rds")
 m3 <- readRDS("../data/censored-mle-m3.rds")
 m4 <- readRDS("../data/censored-mle-m4.rds")
 imps <- readRDS("../data/imputations.rds")
+biofoul <- readRDS("../data/biofouling.rds")
+vessels <- biofoul %>% distinct(boatID, .keep_all = TRUE)
+################################################################################
+################################################################################
+
+################################################################################
+################################################################################
+## Begin Section: Observed weight figures
+################################################################################
+################################################################################
+histData <- biofoul %>%
+    filter(wetWeight >= 1.5) %>%
+    mutate(wwLog = log(wetWeight)) %>%
+    select(LocID, `Weight (gm)` = wetWeight,
+           `Weight (gm), log-scale` = wwLog) %>%
+    gather("logged", "ww", 2:3)
+plHist <- ggplot(histData, aes(x = ww)) + geom_histogram(bins = 11) +
+    facet_grid(LocID ~ logged, scales = "free_x") +
+    xlab("") +
+    ylab("Count") +
+    theme(panel.grid.major.x = element_blank(),
+          panel.grid.minor.x = element_blank())
+ggsave("../graphics/obs-hist.pdf", plHist)
+################################################################################
+################################################################################
+
+################################################################################
+################################################################################
+## Begin Section: Imputation figures
+################################################################################
+################################################################################
+lvl2Imp <- lapply(imps, function(x) x$lvl2) %>% bind_rows() %>%
+    mutate(nummi = rep(seq_along(imps), each = nrow(vessels)))
+vessImps <- vessels %>%
+    mutate(
+        days1 = as.numeric(scale(days1)),
+        days2 = as.numeric(scale(days2)),
+        midTrips = as.numeric(scale(midTrips)),
+        ApproxHullSA = as.numeric(scale(ApproxHullSA)),
+        nummi = 0
+    )
+vessImps <- bind_rows(vessImps, lvl2Imp)
+plDays1 <- ggplot(vessImps, aes(x = factor(nummi), y = days1)) +
+    geom_boxplot(outlier.size = 0.5) +
+    xlab("Imputation number") +
+    ylab("") +
+    theme_bw(base_size = 7.7) +
+    theme(axis.text.x = element_blank(),
+          axis.ticks.x = element_blank(),
+          panel.grid.major.x = element_blank(),
+          panel.grid.minor.x = element_blank())
+ggsave("../graphics/imp-days1.pdf", plDays1, width = 4.9, height = 4.9)
+plMidTrips <- ggplot(vessImps, aes(x = factor(nummi), y = midTrips)) +
+    geom_boxplot(outlier.size = 0.5) +
+    xlab("Imputation number") +
+    ylab("") +
+    theme_bw(base_size = 7.7) +
+    theme(axis.text.x = element_blank(),
+          axis.ticks.x = element_blank(),
+          panel.grid.major.x = element_blank(),
+          panel.grid.minor.x = element_blank())
+ggsave("../graphics/imp-trips.pdf", plMidTrips, width = 4.9, height = 4.9)
+set.seed(76)
+impSelect <- sample(seq_along(imps), 15)
+plPaint <- ggplot(vessImps %>% filter(nummi %in% c(0, impSelect),
+                                      !is.na(paintType)),
+                  aes(x = paintType)) +
+    geom_bar() +
+    facet_wrap(~ factor(nummi)) +
+    xlab("Anti-fouling paint type") +
+    theme_bw(base_size = 7.7) +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1))
+ggsave("../graphics/imp-paint.pdf", plPaint, width = 4.9, height = 4.9)
 ################################################################################
 ################################################################################
 
@@ -52,8 +126,10 @@ plM1boat <- ggplot(a1Dat, aes(x = value, y = mid, ymin = low, ymax = high,
     geom_abline(aes(slope = slope, intercept = intercept), data = slopes) +
     ylab("Vessel-level intercept") +
     xlab("Scaled value") +
+    scale_colour_brewer(palette = "Dark2") +
+    theme_bw(base_size = 7.7) +
     theme(legend.position = "bottom")
-ggsave("../graphics/plM1boat.pdf", plM1boat)
+ggsave("../graphics/plM1boat.pdf", plM1boat, width = 4.9, height = 4.9)
 plM1paint <- ggplot(a1Dat, aes(x = value, y = mid, ymin = low, ymax = high,
                                colour = `Paint type`)) +
     geom_pointrange(fatten = 0.5) +
@@ -61,8 +137,10 @@ plM1paint <- ggplot(a1Dat, aes(x = value, y = mid, ymin = low, ymax = high,
     geom_abline(aes(slope = slope, intercept = intercept), data = slopes) +
     ylab("Vessel-level intercept") +
     xlab("Scaled value") +
+    scale_colour_brewer(palette = "Dark2") +
+    theme_bw(base_size = 7.7) +
     theme(legend.position = "bottom")
-ggsave("../graphics/plM1paint.pdf", plM1paint)
+ggsave("../graphics/plM1paint.pdf", plM1paint, width = 4.9, height = 4.9)
 ################################################################################
 ################################################################################
 
@@ -106,8 +184,9 @@ plM3boat <- ggplot(a3Dat, aes(x = value, y = mid, ymin = low, ymax = high,
                 data = parsType) +
     ylab("Vessel-level intercept") +
     xlab("Scaled value") +
+    scale_colour_brewer(palette = "Dark2") +
     theme(legend.position = "bottom")
-ggsave("../graphics/plM3boat.pdf", plM3boat)
+ggsave("../graphics/plM3boat.pdf", plM3boat, height = 3.5)
 intPaint <- c(0, median(slopes$betaPaint[,1]), median(slopes$betaPaint[,2]))
 slopeTripsPaint <- c(median(slopes$betaMidTrips),
                      median(slopes$betaMidTrips + slopes$betaTripsPaint[,1]),
@@ -120,13 +199,16 @@ plM3paint <- ggplot(a3Dat %>% filter(type == "Median number of trips"),
                     aes(x = value, y = mid, ymin = low, ymax = high,
                         colour = `Paint type`)) +
     geom_pointrange(fatten = 1) +
+    facet_wrap(~ type) +
     geom_abline(aes(slope = slope, intercept = intercept,
                     colour = `Paint type`),
                 data = parsType) +
     ylab("Vessel-level intercept") +
     xlab("Scaled value") +
+    scale_colour_brewer(palette = "Dark2") +
+    guides(colour = guide_legend(nrow = 2, byrow = TRUE)) +
     theme(legend.position = "bottom")
-ggsave("../graphics/plM3paint.pdf", plM3paint)
+ggsave("../graphics/plM3paint.pdf", plM3paint, width = 3.5, height = 3.5)
 ################################################################################
 ################################################################################
 
@@ -144,8 +226,7 @@ a4Dat <- left_join(a4Dat, lvl2) %>%
     gather(type, value, -low, -mid, -high, -boatID, -paintType, -boatType) %>%
     mutate(type = recode(type,
                          days1 = "Days since last used",
-                         days2 = "Days since last cleaned",
-                         midTrips = "Median number of trips")) %>%
+                         days2 = "Days since last cleaned")) %>%
     rename(`Vessel type` = boatType, `Paint type` = paintType)
 slopes <- extract(m4, c("betaDays1", "betaDays2", "betaType",
                         "betaDaysType"))
@@ -153,29 +234,50 @@ intType <- c(0, median(slopes$betaType[,1]), median(slopes$betaType[,2]))
 slopeDaysType <- c(median(slopes$betaDays1),
                    median(slopes$betaDays1 + slopes$betaDaysType[,1]),
                    median(slopes$betaDays1 + slopes$betaDaysType[,2]))
-parsType <- tibble(intercept = intType, slope = slopeDaysType,
-                   `Vessel type` = factor(levels(a4Dat$`Vessel type`)),
-                   type = rep("Days since last used", 3))
-plM4type <- ggplot(a4Dat %>% filter(type == "Days since last used"),
+parsType <- tibble(intercept = rep(intType, 2),
+                   slope = c(slopeDaysType, rep(median(slopes$betaDays2), 3)),
+                   `Vessel type` = factor(rep(levels(a4Dat$`Vessel type`), 2)),
+                   type = c(rep("Days since last used", 3),
+                            rep("Days since last cleaned", 3)))
+parsPaint <- tibble(intercept = intType,
+                    slope = rep(median(slopes$betaDays2), 3),
+                    `Vessel type` = factor(levels(a4Dat$`Vessel type`)),
+                    type = rep("Days since last cleaned"))
+plM4type <- ggplot(a4Dat,
                    aes(x = value, y = mid, ymin = low, ymax = high,
                        colour = `Vessel type`)) +
     geom_pointrange(fatten = 0.5) +
+    facet_wrap(~ type) +
     geom_abline(aes(slope = slope, intercept = intercept,
                     colour = `Vessel type`),
                 data = parsType) +
     ylab("Vessel-level intercept") +
     xlab("Scaled value") +
+    scale_colour_brewer(palette = "Dark2") +
     theme(legend.position = "bottom")
-ggsave("../graphics/plM4type.pdf", plM4type)
-plM4days2 <- ggplot(a4Dat %>% filter(type == "Days since last cleaned"),
-                    aes(x = value, y = mid, ymin = low, ymax = high,
-                        colour = `Vessel type`)) +
-    geom_pointrange(fatten = 0.5) +
-    geom_abline(aes(slope = median(slopes$betaDays2), intercept = intercept,
-                    colour = `Vessel type`), data = parsType) +
-    ylab("Vessel-level intercept") +
-    xlab("Scaled value") +
-    theme(legend.position = "bottom")
-ggsave("../graphics/plM4days2.pdf", plM4days2)
+ggsave("../graphics/plM4type.pdf", plM4type, height = 3.5)
 ################################################################################
 ################################################################################
+
+################################################################################
+################################################################################
+## Begin Section: Regression coefficients
+################################################################################
+################################################################################
+
+## ## Something like the following:
+## plotData <- coefs %>%
+##         filter(lvl == lookupData$lvl[i]) %>%
+##         mutate(label = factor(label, levels = ords)) %>%
+##         group_by(model) %>%
+##         arrange(label) %>%
+##         filter(est < 10, est > -10, lower > -10)
+##     plComp <- ggplot(
+##         plotData, aes(x = label, y = est, ymin = lower, ymax = upper, colour =
+##                           model)
+##     ) +
+##         geom_pointrange(position = position_dodge(width = 0.75)) +
+##         geom_hline(yintercept = 0, linetype = 2) +
+##         coord_flip() +
+##         ylab("log(Coefficient)") +
+##         xlab("Variable")
